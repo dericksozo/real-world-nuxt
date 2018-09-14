487eba48b3a05e720ddc578798cdf5b1c9c7cfb5
Derick Ruiz <derick.r.ruiz@gmail.com>
2018-09-14 13:12:54 +0700

    Changing the links from router-links into nuxt-links for the nuxt app.

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="nav" class="nav">
-    <router-link to="/" class="brand">Real World Events</router-link>
+    <nuxt-link to="/" class="brand">Real World Events</nuxt-link>
     <nav>
-      <router-link :to="{ name: 'event-list' }">List</router-link> |
-      <router-link :to="{ name: 'event-create' }">Create</router-link>
+      <nuxt-link :to="{ name: 'event-list' }">List</nuxt-link> |
+      <nuxt-link :to="{ name: 'event-create' }">Create</nuxt-link>
     </nav>
   </div>
 </template>
