a9a03f13a799ce5331f4790b3e08093d4d83035d
Derick Ruiz <derick.r.ruiz@gmail.com>
2018-10-02 19:29:59 +0700

    Upating the default layout so that it shows the notifications everywhere.

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,12 +8,12 @@
 
 <script>
 import NavBar from '@/components/NavBar.vue'
-// import NotificationContainer from '@/components/notifications/NotificationContainer.vue'
+import NotificationContainer from '@/components/NotificationContainer.vue'
 
 export default {
   components: {
     NavBar,
-    // NotificationContainer
+    NotificationContainer
   }
 }
 </script>
