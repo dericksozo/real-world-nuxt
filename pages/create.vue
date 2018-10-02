e07c5ffc464b42969f4b856cfd4b543d4abf9d15
Derick Ruiz <derick.r.ruiz@gmail.com>
2018-10-02 19:32:13 +0700

    Updating the create page so that you can actually create an event.

--- pages/create.vue
+++ pages/create.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an event</h1>
+    <Form/>
+  </div>
 </template>
+
+<script>
+import Form from '@/components/form/Form.vue'
+
+export default {
+  name: 'Create',
+  components: {
+    Form
+  }
+}
+</script>
