e4dd5ae188c1e7abdc856eaf7d7194f5c71cafc6
Derick Sozo <derick.r.ruiz@gmail.com>
2018-10-20 13:54:28 +0700

    Adding the routes, getting posts, and ability to add a post.

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 
+const routes = require('./routes.js');
+
 const app = express();
 
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('combined'));
+
+app.use('/micro-posts', routes);
 
 app.listen(8081, () => {
   console.log("Listening on 8081");
